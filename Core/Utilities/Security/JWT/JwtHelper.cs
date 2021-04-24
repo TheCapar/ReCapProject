5474b6e6a13647161892d72946771810a0e9bc6c
TheCapar <deha capar@DESKTOP-IBU3JE1.(none)>
2021-04-24 17:17:24 +0300

    TokenOptions hatası devam etmekte fakat Cache, Transaction ve Performance aspectleri eklendi

--- Core/Utilities/Security/JWT/JwtHelper.cs
+++ Core/Utilities/Security/JWT/JwtHelper.cs
@@ -17,18 +17,22 @@ namespace Core.Utilities.Security.JWT
         public IConfiguration Configuration { get; }
         private TokenOptions _tokenOptions;
         private DateTime _accessTokenExpiration;
+
         public JwtHelper(IConfiguration configuration)
+
         {
             Configuration = configuration;
             _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
-
         }
+
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
             _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
             var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
             var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+
             var jwt = CreateJwtSecurityToken(_tokenOptions, user, signingCredentials, operationClaims);
+
             var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
             var token = jwtSecurityTokenHandler.WriteToken(jwt);
 
@@ -40,8 +44,7 @@ namespace Core.Utilities.Security.JWT
 
         }
 
-        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user,
-            SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
+        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user, SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
         {
             var jwt = new JwtSecurityToken(
                 issuer: tokenOptions.Issuer,
@@ -50,8 +53,9 @@ namespace Core.Utilities.Security.JWT
                 notBefore: DateTime.Now,
                 claims: SetClaims(user, operationClaims),
                 signingCredentials: signingCredentials
-            );
+                );
             return jwt;
+
         }
 
         private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
@@ -61,8 +65,8 @@ namespace Core.Utilities.Security.JWT
             claims.AddEmail(user.Email);
             claims.AddName($"{user.FirstName} {user.LastName}");
             claims.AddRoles(operationClaims.Select(c => c.Name).ToArray());
-
             return claims;
+
         }
     }
 }
